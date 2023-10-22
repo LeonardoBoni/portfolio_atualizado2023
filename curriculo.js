0683196a45150da62043e2d00e819efe5bc1a487
LeonardoBoni <123012608+LeonardoBoni@users.noreply.github.com>
2023-10-22 05:06:54 +0000

    teste

--- curriculo.js
+++ curriculo.js
@@ -1,31 +1,15 @@
 function home(){
-    window.location.href = 'index.html'
+    window.location.href = 'index.html';
 }
 
 function sobremim(){
-window.location.href = 'sobremim.html'
+    window.location.href = 'sobremim.html'
 }
 
 function meustrabalhos(){
-    window.location.href = 'meustrabalhos.html'
+    window.location.href = 'trabalhos.html'
 }
 
-function objetivoprofi(){
-    window.location.href = 'objetivo.html'
-}
-
-
-function dadospessoais(){
-    var res = document.getElementById('ressobremim')
-    res.innerHTML = '<div id = "persores"><h1 id = "titulosobremim">NOME:</h1><p id = "txtsobremim">Leonardo Rodrigues Boni</p><h1 id = "titulosobremim">DATA DE NASCIMENTO:</h1><p id = "txtsobremim">16/11/2005</p><h1 id = "titulosobremim">TELEFONE:</h1><p id = "txtsobremim">+55(18) 99661-0633</p><h1 id = "titulosobremim">EMAIL:</h1><p id = "txtsobremim">leoboni_xbox360@hotmail.com</p></div>'
-}
-
-function formacao(){
-    var res = document.getElementById('ressobremim')
-    res.innerHTML = '<div id = "resformacao"><h1 id = "titulosobremim">ESCOLARIDADE:</h1><p id = "txtsobremim">Ensino fundamental✔️</p><p id = "txtsobremim">Ensino médio✔️</p><h1 id = "titulosobremim">CURSOS TÉCNICOS:</h1><p id = "txtsobremim">Eletroeletrônica(SENAI)✔️</p><h1 id = "titulosobremim">CURSOS:</h1><p id = "txtsobremim">Privacidade e proteção de dados(LGPD)✔️</p><p id = "txtsobremim">Competência transversal-Segurança no trabalho✔️</p><p id = "txtsobremim">Excel básico✔️</p><p id = "txtsobremim">Implementação de serviços de inteligência artificial em nuvem - Microsoft AI 900✔️</p><p id = "txtsobremim">Desvendando a indústria 4.0(SENAI)✔️</p><p id = "txtsobremim">Preparação para o mundo do trabalho(SENAI)✔️</p><p id = "txtsobremim">Crie um site simples usando html,css e JavaScript(BRADESCO)✔️</p></div>'
-}
-
-function estrangeira(){
-    var res = document.getElementById('ressobremim')
-    res.innerHTML = '<div id = "resestrangeira"><h1 id = "titulosobremim">CURSO DE INGLÊS:</h1><p id = "txtsobremim">Atualmente, tenho 4 anos de curso de língua Inglesa, em uma escola particular de Araçatuba,"Jay English Studio", com nível considerado avançado!</p></div>'
+function ogjprofissional(){
+    window.location.href = 'objt.html'
 }
